83b2fbf3bb77dcd963857017b19b9077431d1ca2
S.Percentage <Syn.Tri.Naga@gmail.com>
2022-09-18 13:41:17 +0900

    fix: objects can have ownerships

--- src/block.rs
+++ src/block.rs
@@ -1,21 +1,23 @@
+use std::borrow::Cow;
+
 #[derive(serde::Serialize)]
 #[serde(tag = "type", rename_all = "snake_case")]
 pub enum Block<'s> {
     Divider {
         #[serde(skip_serializing_if = "Option::is_none")]
-        block_id: Option<&'s str>,
+        block_id: Option<Cow<'s, str>>,
     },
     Header {
         /// Note: only available PlainText object
         text: TextObject<'s>,
         #[serde(skip_serializing_if = "Option::is_none")]
-        block_id: Option<&'s str>,
+        block_id: Option<Cow<'s, str>>,
     },
     Section {
         #[serde(skip_serializing_if = "Option::is_none")]
         text: Option<TextObject<'s>>,
         #[serde(skip_serializing_if = "Option::is_none")]
-        block_id: Option<&'s str>,
+        block_id: Option<Cow<'s, str>>,
         #[serde(skip_serializing_if = "Vec::is_empty")]
         fields: Vec<TextObject<'s>>,
     },
@@ -26,12 +28,12 @@ pub enum Block<'s> {
 pub enum TextObject<'s> {
     #[serde(rename = "mrkdwn")]
     Markdown {
-        text: &'s str,
+        text: Cow<'s, str>,
         #[serde(skip_serializing_if = "Option::is_none")]
         verbatim: Option<bool>,
     },
     PlainText {
-        text: &'s str,
+        text: Cow<'s, str>,
         #[serde(skip_serializing_if = "Option::is_none")]
         emoji: Option<bool>,
     },
